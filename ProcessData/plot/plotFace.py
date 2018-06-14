94a304d8f65402c93a47848a6b43a699060a4ac2
BrunoBob <tessierb95@gmail.com>
2018-06-14 17:14:16 +0900

    start removing rotation from 3d data

--- ProcessData/plot/plotFace.py
+++ ProcessData/plot/plotFace.py
@@ -10,7 +10,7 @@ def plot2D():
 	color = ['bo', 'go','ro', 'co','mo', 'yo','ko', 'bv','gv','rv', 'cv','mv', 'yv','kv','gs','rs', 'cs','ms', 'ys','ks',]
 	for graphNb in range(0,num):
 		fig = plt.figure()
-		plt.axis([-200, 200, 850, 1300])
+		plt.axis([-250, 250, -200, 120])
 		for marker in range(0,NB_MARKER):
 			plt.plot(X[graphNb][marker],Y[graphNb][marker], color[marker])
 
@@ -32,11 +32,25 @@ def plot3D():
 		fig.savefig(name)
 		plt.close(fig)
 
+def correctMovement(X, Y, Z, num):
+	max = 0
+	min = 10000
+	v1 = 0
+	for row in range(0,num):
+		dist12 = math.sqrt(((X[row][REF_CENTER_COL]-X[row][REF_UP_COL])**2) +((Y[row][REF_CENTER_COL]-Y[row][REF_UP_COL])**2) +((Z[row][REF_CENTER_COL]-Z[row][REF_UP_COL])**2))
+		if(dist12 > max):
+			max = dist12
+		if(dist12 < min):
+			min = dist12
+	print(min)
+	print(max)
+	print(max-min)
 
 FILE_PATH = "../../Data/Bruno_1_juin/"
 START_LINE = 581
 END_LINE = 52021
-FREQ = 6
+#52021 1781
+FREQ = 100#6
 NB_MARKER = 19
 REF_CENTER_COL = 2
 REF_LEFT_COL = 1
@@ -57,29 +71,38 @@ Z = np.zeros((num,NB_MARKER))
 #Ignore the header lines
 for iter in range(0,START_LINE-1):
 	line = next(reader)
-
+print(FREQ)
 #Read the data and save them in a matrix
-for row in range(START_LINE, END_LINE-START_LINE , FREQ):
+for row in range(START_LINE, END_LINE , FREQ):
 	line = next(reader)
 	while(line[2]==""):
 		line = next(reader)
-	print(row)
+	#print(row)
 	for axe in range(0,NB_MARKER):
-		
 		if(line[axe*3 +2] != ""):
-			X[(row / FREQ) - 1][axe] = float(line[axe*3 + 2])
+			X[((row-START_LINE) / FREQ) - 1][axe] = float(line[axe*3 + 2])
 		if(line[axe*3 +3] != ""):
-			Y[row / FREQ - 1][axe] = float(line[axe*3 + 3])
+			Y[(row-START_LINE) / FREQ - 1][axe] = float(line[axe*3 + 3])
 		if(line[axe*3 +4] != ""):
-			Z[row / FREQ - 1][axe] = float(line[axe * 3 + 4])
+			Z[(row-START_LINE) / FREQ - 1][axe] = float(line[axe * 3 + 4])
 	
 	if(reader.line_num + FREQ <= END_LINE):	
 		for iter in range(0,FREQ-1):
 			line = next(reader)
 
+#Center the data on reference point
+
+""" for row in range(0,num):
+	defX = X[row][REF_CENTER_COL] #- X[0][REF_CENTER_COL]
+	defY = Y[row][REF_CENTER_COL] #- Y[0][REF_CENTER_COL]
+	for axe in range(0,NB_MARKER):
+		X[row][axe] = X[row][axe] - defX
+		Y[row][axe] = Y[row][axe] - defY """
+
 print("Creat graph")
 #plot the data
-plot2D()
+correctMovement(X,Y,Z,num)
+#plot2D()
 
 	
 print('end')
