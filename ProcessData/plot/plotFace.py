b903c27aedb075586b6ab8a872062ef02f85d640
BrunoBob <tessierb95@gmail.com>
2018-07-02 16:43:24 +0900

    correctMovement fct work

--- ProcessData/plot/plotFace.py
+++ ProcessData/plot/plotFace.py
@@ -5,12 +5,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+FILE_PATH = "../../Data/Bruno_1_juin/"
+START_LINE = 581
+END_LINE = 1781
+#52021 1781
+FREQ = 10
+NB_MARKER = 19
+REF_CENTER_COL = 2
+REF_LEFT_COL = 1
+REF_UP_COL = 0
+
 #Plot 2D graph function
-def plot2D():
+def plot2D(X, Y, Z, num):
 	color = ['bo', 'go','ro', 'co','mo', 'yo','ko', 'bv','gv','rv', 'cv','mv', 'yv','kv','gs','rs', 'cs','ms', 'ys','ks',]
+
 	for graphNb in range(0,num):
 		fig = plt.figure()
 		plt.axis([-250, 250, -200, 120])
+		#plt.axis([-200, 200, 900, 1300])
+		
 		for marker in range(0,NB_MARKER):
 			plt.plot(X[graphNb][marker],Y[graphNb][marker], color[marker])
 
@@ -20,7 +33,7 @@ def plot2D():
 		plt.close(fig)
 
 #Plot 3D graph function
-def plot3D():
+def plot3D(X, Y, Z, num):
 	for graphNb in range(0,num):
 		fig = plt.figure()
 		plt.axis([-100, 100, 1000, 1300])
@@ -33,44 +46,51 @@ def plot3D():
 		plt.close(fig)
 
 def correctMovement(X, Y, Z, num):
-	Vinit = [X[0][REF_UP_COL] - X[0][REF_CENTER_COL], Y[0][REF_UP_COL] - Y[0][REF_CENTER_COL], Z[0][REF_UP_COL] - Z[0][REF_CENTER_COL]]
-	Vcur = [X[1][REF_UP_COL] - X[1][REF_CENTER_COL], Y[1][REF_UP_COL] - Y[1][REF_CENTER_COL], Z[1][REF_UP_COL] - Z[1][REF_CENTER_COL]]
-	identity = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-
-	#print(np.matrix(Vtest))
-	#print(np.matrix(Vcur))
-
-	#normalisation
-	Dinit = np.linalg.norm(Vinit)
-	Dnew = np.linalg.norm(Vcur)
-	Vinit = Vinit / Dinit
-	Vcur = Vcur / Dnew
-
-	cross = np.cross(Vcur,Vinit)
-	dot = np.dot(Vcur,Vinit)
-	norm = np.linalg.norm(cross)
-	ssc = np.array([(0, -cross[2], cross[1]), (cross[2], 0, -cross[0]), (-cross[1], cross[0], 0)])
-	ssc2 = np.matmul(ssc, ssc)
 	
-	#Compute the rotation matrix between vectro Vinit AND vCUR
-	R = (1-dot)/(norm*norm)
-	R = ssc2 * R
-	R = ssc + R
-	R = identity + R
+	#Get reference vector and normalisation
+	Vinit = [X[0][REF_UP_COL] - X[0][REF_CENTER_COL], Y[0][REF_UP_COL] - Y[0][REF_CENTER_COL], Z[0][REF_UP_COL] - Z[0][REF_CENTER_COL]]
+	Dinit = np.linalg.norm(Vinit)
+	Vinit = Vinit / Dinit
+	
+	#for each image
+	for graphNb in range(1,num):
 
-	print(np.matrix(R))
-	print(np.matrix(np.matmul(R,Vcur)*Dinit))
+		#Get current vector and normalisation
+		Vcur = [X[graphNb][REF_UP_COL] - X[graphNb][REF_CENTER_COL], Y[graphNb][REF_UP_COL] - Y[graphNb][REF_CENTER_COL], Z[graphNb][REF_UP_COL] - Z[graphNb][REF_CENTER_COL]]
+		Dcur = np.linalg.norm(Vcur)
+		Vcur = Vcur / Dcur
 
+		#Compute the rotation matrix between vectro Vinit AND vCUR
+		identity = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+		cross = np.cross(Vcur,Vinit)
+		dot = np.dot(Vcur,Vinit)
+		norm = np.linalg.norm(cross)
+		ssc = np.array([(0, -cross[2], cross[1]), (cross[2], 0, -cross[0]), (-cross[1], cross[0], 0)])
+		ssc2 = np.matmul(ssc, ssc)
+		R = (1-dot)/(norm*norm)
+		R = ssc2 * R
+		R = ssc + R
+		R = identity + R
 
-FILE_PATH = "../../Data/Bruno_1_juin/"
-START_LINE = 581
-END_LINE = 52021
-#52021 1781
-FREQ = 100#6
-NB_MARKER = 19
-REF_CENTER_COL = 2
-REF_LEFT_COL = 1
-REF_UP_COL = 0
+		#Remove the rotation for each marker
+		for marker in range(0,NB_MARKER):
+			point = [X[graphNb][marker], Y[graphNb][marker], Z[graphNb][marker]]
+			#print(np.matrix(point))
+			point = np.matmul(R,point)
+			#print(np.matrix(point))
+			X[graphNb][marker] = point[0]
+			Y[graphNb][marker] = point[1]
+			Z[graphNb][marker] = point[2]
+
+		#remove translation
+		defX = X[graphNb][REF_CENTER_COL] #- X[0][REF_CENTER_COL]
+		defY = Y[graphNb][REF_CENTER_COL] #- Y[0][REF_CENTER_COL]
+		defZ = Z[graphNb][REF_CENTER_COL] #- Z[0][REF_CENTER_COL]
+		for marker in range(0,NB_MARKER):
+			X[graphNb][marker] = X[graphNb][marker] - defX
+			Y[graphNb][marker] = Y[graphNb][marker] - defY
+			Z[graphNb][marker] = Z[graphNb][marker] - defZ
+
 
 print("start")
 
@@ -87,22 +107,21 @@ Z = np.zeros((num,NB_MARKER))
 #Ignore the header lines
 for iter in range(0,START_LINE-1):
 	line = next(reader)
-print(FREQ)
+
 #Read the data and save them in a matrix
 for row in range(START_LINE, END_LINE , FREQ):
 	line = next(reader)
 	while(line[2]==""):
 		line = next(reader)
-	#print(row)
+
 	for axe in range(0,NB_MARKER):
 		if(line[axe*3 +2] != ""):
-			X[((row-START_LINE) / FREQ) - 1][axe] = float(line[axe*3 + 2])
+			X[((row-START_LINE) / FREQ) ][axe] = float(line[axe*3 + 2])
 		if(line[axe*3 +3] != ""):
-			Y[(row-START_LINE) / FREQ - 1][axe] = float(line[axe*3 + 3])
+			Y[(row-START_LINE) / FREQ][axe] = float(line[axe*3 + 3])
 		if(line[axe*3 +4] != ""):
-			Z[(row-START_LINE) / FREQ - 1][axe] = float(line[axe * 3 + 4])
-	
-	if(reader.line_num + FREQ <= END_LINE):	
+			Z[(row-START_LINE) / FREQ][axe] = float(line[axe * 3 + 4])
+	if(reader.line_num + FREQ <= END_LINE):
 		for iter in range(0,FREQ-1):
 			line = next(reader)
 
@@ -118,7 +137,7 @@ for row in range(START_LINE, END_LINE , FREQ):
 print("Creat graph")
 #plot the data
 correctMovement(X,Y,Z,num)
-#plot2D()
+plot2D(X,Y,Z,num)
 
 	
 print('end')
